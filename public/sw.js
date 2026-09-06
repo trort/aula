b6033d26a3d8e35cea10400cc435a0e12f3b6e76
jj.clawd <jj.clawd@localhost>
2026-09-06 16:36:01 -0400

    feat: add 39 supplementary common chars (deduped) with confusables and voice assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kids-learning-companion-v9";
+const CACHE_NAME = "kids-learning-companion-v10";
 const PRECACHE = ["./", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
